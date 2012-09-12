b4335ac67bf466a24e87385bde11de9d50ac5258
Edouard Brunvarlet <brunva_e@linux-8ba4.site>
2012-09-12 14:24:23 +0200

    add Client

---
+++ src/Client/Code-A-Client/Code-A-Client.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-09-12T13:23:39
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = Code-A-Client
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ src/Client/Code-A-Client/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    NewGame();
+}
+
+void MainWindow::NewGame()
+{
+    std::cout << "You just started a new game" << std::endl;
+}

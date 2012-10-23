153c0b4542df6c2b56e1ef0e7262d12ca911827b
ledrap_b <jebik@hotmail.fr>
2012-10-23 19:57:49 +0200

    thoughts on amazing master server interface #2 :D

---
+++ src/MasterServer/include/IAccountManager.hpp
@@ -0,0 +1,35 @@
+//
+// IDataManager.hpp for  in /home/berger_t/PFA/src/MasterServer/src
+// 
+// Made by thierry berger
+// Login   <berger_t@epitech.net>
+// 
+// Started on  Tue Oct 23 14:13:05 2012 thierry berger
+// Last update Tue Oct 23 15:05:06 2012 thierry berger
+//
+
+#ifndef IACCOUNT_MANAGER_HPP_
+# define IACCOUNT_MANAGER_HPP_
+#include "IAccount.hpp"
+
+template<typename T>
+class	IAccountManager
+{
+public:
+
+  bool initialize(bool (*serializer)(T data)) = 0; // true if succeed
+  IAccount<T>* createAccount(const std::string& login, const std::string& passwd) = 0; // TODO: add mail
+  bool deleteAccount(const IAccount<T>&) = 0;
+  
+  bool updateAccount(const IAccount<T>&) = 0;
+  IAccount<T>* tryConnect(const std::string& login, const std::string& passwd) = 0;
+private:
+  class IDataManager
+  {
+    bool setAccount(Account& account) = 0;
+    bool save(T data) = 0;
+    T load() = 0;
+  };
+};
+
+#endif

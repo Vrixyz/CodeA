2e95a37c0d5b9a626d408d1eb142c5477762a700
leurquin mathieu <mathieu.leurquin@epitech.eu>
2013-02-26 20:47:25 +0100

    proute

--- src/Server/Portal.cpp
+++ src/Server/Portal.cpp
@@ -5,7 +5,7 @@
 // Login   <leurqu_m@epitech.net>
 // 
 // Started on  Thu Feb 21 10:56:20 2013 mathieu leurquin
-// Last update Fri Feb 22 12:35:55 2013 mathieu leurquin
+// Last update Mon Feb 25 16:19:43 2013 mathieu leurquin
 //
 
 #include "World.hpp"
@@ -91,9 +91,9 @@ void Server::Portal::createMinion()
   BitField *b;
 
   if (_world.players.size() == 1)
-    b = new  BitField(Server::BitField::TEAM1_UNIT, Server::BitField::TEAM2_BULLET | Server::BitField::TEAM2_UNIT | Server::BitField::OBSTACLE | Server::BitField::PORTAL);
+    b = new  BitField(Server::BitField::TEAM1_UNIT, Server::BitField::TEAM2_BULLET | Server::BitField::TEAM2_UNIT | Server::BitField::OBSTACLE | Server::BitField::PORTAL | Server::BitField::TEAM2_SHIELD);
   else
-    b = new  BitField(Server::BitField::TEAM2_UNIT, Server::BitField::TEAM1_BULLET | Server::BitField::TEAM1_UNIT | Server::BitField::OBSTACLE | Server::BitField::PORTAL | Server::BitField::TEAM2_UNIT);
+    b = new  BitField(Server::BitField::TEAM2_UNIT, Server::BitField::TEAM1_BULLET | Server::BitField::TEAM1_UNIT | Server::BitField::OBSTACLE | Server::BitField::PORTAL | Server::BitField::TEAM2_UNIT | Server::BitField::TEAM1_SHIELD);
 
   b2Vec2 position = this->getBody()->GetPosition();
   if (belongsToPlayer(0) == true)

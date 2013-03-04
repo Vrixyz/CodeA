3aae07e27bd29c1bfef94908273f115b92226717
ledrap_b <ledrap_b@ledrap-b.(none)>
2013-03-04 14:33:29 +0100

    DORIAN NOOB

--- src/MasterServer/client_test/AccWindow.cpp
+++ src/MasterServer/client_test/AccWindow.cpp
@@ -1,4 +1,5 @@
 #include "AccWindow.h"
+#include "ShowWarning.h"
 
 AccWindow::AccWindow(int size_x, int size_y, MyWindow *parent) : QDialog(parent, Qt::FramelessWindowHint)
 {
@@ -95,9 +96,12 @@ void    AccWindow::RecvInfosClient()
         else if (idData == MasterData::Command::ERROR)
         {
             MasterData::ErrorMsg err("");
+	    ShowWarning*	 error;
+
             pac.next(&result);
             result.get().convert(&err);
-            std::cerr << "Erreur connexion : " << err.msg << std::endl;
+	    error = new ShowWarning(_parent, err.msg);
+	    error->show();
         }
     }
 }
